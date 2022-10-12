0c28a882018395dde5aad99069432f4633d92070
mehmandarova <49618637+mehmandarova@users.noreply.github.com>
2022-10-12 20:03:57 +0200

    usikker, men AspFactor ferdig(?)

--- in2030-oblig-2022/no/uio/ifi/asp/parser/AspFactor.java
+++ in2030-oblig-2022/no/uio/ifi/asp/parser/AspFactor.java
@@ -6,9 +6,31 @@ import no.uio.ifi.asp.scanner.*;
 import static no.uio.ifi.asp.scanner.TokenKind.*;
 
 public class AspFactor extends AspSyntax {
-    
+    ArrayList<AspFactorPrefix> facpre = new ArrayList<>();
+    ArrayList<AspFactorOpr> facopr = new ArrayList<>();
+    ArrayList<AspPrimary> prim = new ArrayList<>();
+
     AspFactor(int n) {
         super(n);
+    }
+
+    public static AspFactor parse(Scanner s) {
+        enterParser("factor");
+        AspFactor fac = new AspFactor(s.curLineNum());
+
+        if (s.isFactorPrefix()) {
+            fac.facpre.add(AspFactorPrefix.parse(s));
+        } else {
+            fac.facpre.add(null);
+        }
+        fac.prim.add(AspPrimary.parse(s));
+
+        if (s.isFactorOpr()) {
+            fac.facopr.add(AspFactorOpr.parse(s));
+        }
+        
+        leaveParser("factor");
+        return fac;
     }
 
     @Override
--- in2030-oblig-2022/no/uio/ifi/asp/parser/AspTerm.java
+++ in2030-oblig-2022/no/uio/ifi/asp/parser/AspTerm.java
@@ -6,7 +6,8 @@ import no.uio.ifi.asp.scanner.*;
 import static no.uio.ifi.asp.scanner.TokenKind.*;
 
 public class AspTerm extends AspSyntax {
-    
+    ArrayList<AspFactor> fac = new ArrayList<>();
+    ArrayList<AspTermOpr> teropr = new ArrayList<>();
 
     AspTerm(int n) {
         super(n);
